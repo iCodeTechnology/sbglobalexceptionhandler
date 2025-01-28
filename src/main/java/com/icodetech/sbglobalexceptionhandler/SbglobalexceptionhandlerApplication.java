232fffb72d41e66dd5b2299ea58574304b3a52c1
aniket6838 <aniket.jadhav@4fin.in>
2025-01-28 15:15:23 +0530

    maven.pkg.github.com repository Added.

--- src/main/java/com/icodetech/sbglobalexceptionhandler/SbglobalexceptionhandlerApplication.java
+++ src/main/java/com/icodetech/sbglobalexceptionhandler/SbglobalexceptionhandlerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.icodetech.sbglobalexceptionhandler.*")
 public class SbglobalexceptionhandlerApplication {
 
 	public static void main(String[] args) {
