7ea0a7958e2c281c3ac0d93bfd0c60d170a39b8b
aniket6838 <aniket.jadhav@4fin.in>
2025-01-28 15:55:07 +0530

    Changes.

--- src/main/java/com/icodetech/sbglobalexceptionhandler/SbglobalexceptionhandlerApplication.java
+++ src/main/java/com/icodetech/sbglobalexceptionhandler/SbglobalexceptionhandlerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.icodetech.sbglobalexceptionhandler.*")
 public class SbglobalexceptionhandlerApplication {
 
 	public static void main(String[] args) {
