e0f57d3bb4e06750994eb86a76530b05b1126a90
aniket6838 <aniket.jadhav@4fin.in>
2026-01-25 20:20:59 +0530

    build: Add `spring-boot-starter-test` dependency, update distribution management repository URL, and specify `GlobalExceptionAutoConfiguration` for `SpringBootTest`.

--- src/test/java/com/ajglobaltechnology/sbglobalexceptionhandler/SbglobalexceptionhandlerApplicationTests.java
+++ src/test/java/com/ajglobaltechnology/sbglobalexceptionhandler/SbglobalexceptionhandlerApplicationTests.java
@@ -3,7 +3,7 @@ package com.ajglobaltechnology.sbglobalexceptionhandler;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.ajglobaltechnology.sbglobalexceptionhandler.core.config.GlobalExceptionAutoConfiguration.class)
 class SbglobalexceptionhandlerApplicationTests {
 
 	@Test
